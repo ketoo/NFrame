e7f66b4e84882363b18c5045cfe99a4a9d006171
kytooooo <342006@qq.com>
2015-11-09 15:27:43 +0800

    add component class

---
+++ NFComm/NFPluginModule/NFIComponent.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NFIComponent.cs">
+//     Copyright (C) 2015-2015 lvsheng.huang <https://github.com/ketoo/NFActor>
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+namespace NFrame
+{
+    public class NFIComponent : NFBehaviour
+    {
+
+    }
+}
