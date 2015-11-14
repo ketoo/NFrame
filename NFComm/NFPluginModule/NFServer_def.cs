8765254dc78a87d452e19648ad38e12850b00d02
zhangf911 <zhangf911@gmail.com>
2015-11-14 15:12:38 +0800

    1、添加服务器类型枚举到NFComm工程

---
+++ NFComm/NFPluginModule/NFServer_def.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFrame
+{
+    public abstract class NFServer_def
+    {
+        public enum NF_SERVER_TYPES
+        {
+            NF_ST_CLIENT = 0,    // client
+            NF_ST_REDIS = 1,    //
+            NF_ST_MYSQL = 2,    //
+            NF_ST_MASTER = 3,    //
+            NF_ST_LOGIN = 4,    //
+            NF_ST_PROXY = 5,    //
+            NF_ST_GAME = 6,    //
+            NF_ST_WORLD = 7,    //
+
+        };
+    }
+}
